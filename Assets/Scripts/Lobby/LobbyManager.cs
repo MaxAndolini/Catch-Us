2d98a68bc619afa8c09dfa05271b97d75822e5bc
AsrinSila <72813285+AsrinSila@users.noreply.github.com>
2023-01-03 19:11:11 +0300

    Flashlight implementation started

--- Assets/Scripts/Lobby/LobbyManager.cs
+++ Assets/Scripts/Lobby/LobbyManager.cs
@@ -31,7 +31,7 @@ public class LobbyManager : MonoBehaviourPunCallbacks
 
     private void Update()
     {
-        if (PhotonNetwork.IsMasterClient && PhotonNetwork.CurrentRoom.PlayerCount == 2)
+        if (PhotonNetwork.IsMasterClient && PhotonNetwork.CurrentRoom.PlayerCount == 1)
         {
             playButton.SetActive(true);
         }
